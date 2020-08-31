18d0037ef70b307aad5a350660c025091f1de7d8
TylerSchwartzman <tschwartzman.dev@gmail.com>
2020-08-31 14:10:16 -0500

    Added custom layout for SearchController

---
+++ FavoriteRecipes/Views/Layouts/RecipeSearchLayout.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeSearchLayout.swift
+//  FavoriteRecipes
+//
+//  Created by Tyler Schwartzman on 8/31/20.
+//  Copyright © 2020 Tyler_Dev. All rights reserved.
+//
+
+import UIKit
+
+class RecipeSearchLayout: UICollectionViewFlowLayout {
+    
+    var cache: [UICollectionViewLayoutAttributes] = []
+    
+    override func prepare() {
+        super.prepare()
+        cache.removeAll()
+    }
+    
+}
