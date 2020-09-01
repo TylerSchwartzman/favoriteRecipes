8382710a81ca9fecc363057890361444ebb10456
TylerSchwartzman <tschwartzman.dev@gmail.com>
2020-09-01 16:25:44 -0500

    Added custom colors to UIColor+Ext

---
+++ FavoriteRecipes/Extensions/UIColor+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Ext.swift
+//  FavoriteRecipes
+//
+//  Created by Tyler Schwartzman on 8/31/20.
+//  Copyright © 2020 Tyler_Dev. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+        
+    static let gingerRoot = UIColor(red: 234/255, green: 218/255, blue: 162/255, alpha: 1)
+        
+    static let copperRed = UIColor(red: 206/255, green: 108/255, blue: 71/255, alpha: 1)
+            
+}
