288f9185f7ff265fb47b254490532420da199cee
TylerSchwartzman <tschwartzman.dev@gmail.com>
2020-09-09 13:07:44 -0500

    Refactoring code

--- FavoriteRecipes/Controllers/RecipeDetailsController.swift
+++ FavoriteRecipes/Controllers/RecipeDetailsController.swift
@@ -8,14 +8,6 @@
 
 import UIKit
 
-class IndentedLabel: UILabel {
-    override func drawText(in rect: CGRect) {
-        let insets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
-        let customRect = rect.inset(by: insets)
-        super.drawText(in: customRect)
-    }
-}
-
 class RecipeDetailsController: UITableViewController {
     
     let favoritesButton = UIImage(named: "favorites")
