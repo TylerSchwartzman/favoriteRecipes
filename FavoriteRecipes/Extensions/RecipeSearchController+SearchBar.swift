1f2bb62978bdd643587bab531714c004ea7a6a02
TylerSchwartzman <tschwartzman.dev@gmail.com>
2020-10-14 15:01:18 -0500

    Organized SearchBarDelegate into extension

---
+++ FavoriteRecipes/Extensions/RecipeSearchController+SearchBar.swift
@@ -0,0 +1,9 @@
+//
+//  RecipeSearchController+SearchBar.swift
+//  FavoriteRecipes
+//
+//  Created by Tyler Schwartzman on 10/14/20.
+//  Copyright © 2020 Tyler_Dev. All rights reserved.
+//
+
+import Foundation
